e478a69611bd893cd7d179732ca06208c3662915
unknown <abdelhadilakhdim@gmail.com>
2020-04-22 23:29:42 +0000

    actions

---
+++ src/main/java/com/abdelhadi/serverAppStockControlle/ServerAppStockControlleApplication.java
@@ -0,0 +1,13 @@
+package com.abdelhadi.serverAppStockControlle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAppStockControlleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAppStockControlleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdelhadi/serverAppStockControlle/ServerAppStockControlleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdelhadi.serverAppStockControlle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppStockControlleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
